95bba23d7ca25f64ef320553fac69bde580946dd
Assen95 <asenjob95@gmail.com>
2024-08-03 11:48:28 +0300

    added API selection functionality, modified selected information

--- 7.AccessingTheCamera/AccessingTheCamera/Program.cs
+++ 7.AccessingTheCamera/AccessingTheCamera/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using OpenCvSharp;
-// Create a function called GetAvailableCamera() it loops from 1 to 10 with VideoCapture and return a List of available cameras
-// 
+// 1. Cleanup
+// 2. Create a function called GetAvailableCamera() it loops from 1 to 10 with VideoCapture and return a List of available cameras
+// 3. Create a function which returns a list of APIs, use same logic for camera
+// 4. Modify OpenCamera to include GetAPIs() function
 
 namespace AccessingTheCamera
 {
@@ -10,6 +12,7 @@ namespace AccessingTheCamera
     {
         static void Main(string[] args)
         {
+            // Getting Available Cameras
             List<int> availableCameras = GetAvailableCameras();
             if (availableCameras.Count == 0)
             {
@@ -25,6 +28,7 @@ namespace AccessingTheCamera
 
             int enteredCameraNumber = 0;
 
+            // Camera selection
             while (true)
             {
                 Console.WriteLine("Please choose a camera from the list, from 0 to 10.");
@@ -37,7 +41,32 @@ namespace AccessingTheCamera
                 Console.WriteLine("Entered number doesn't correlate to an existing camera. Please choose one from the aforementioned options.");
             }
 
-            OpenCamera(availableCameras[enteredCameraNumber]);
+            // Getting available APIs after camera selection
+            var availableAPIs = GetAPIsForCamera(availableCameras[enteredCameraNumber]);
+
+            Console.WriteLine($"Your selected camera is Camera: {enteredCameraNumber + 1}");
+            Console.WriteLine("API options for the camera:");
+            foreach(var currentAPI in availableAPIs)
+            {
+                Console.WriteLine($"API: {currentAPI} - {availableAPIs.IndexOf(currentAPI) + 1}");
+            }
+
+            // API Selection
+            int selectedAPINumber = 0;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out selectedAPINumber))
+                {
+                    selectedAPINumber--;
+                    break;
+                }
+
+                Console.WriteLine("The selection does not correlate to an available API for the camera! Please enter of the listed options.");
+            }
+
+
+
+            OpenCamera(availableCameras[enteredCameraNumber], availableAPIs[selectedAPINumber]);
 
 
         }
@@ -58,9 +87,35 @@ namespace AccessingTheCamera
             return availableCameras;
         }
 
-        static void OpenCamera(int selectedCamera) // Add API option later VideoCapture APIs
+        static List<VideoCaptureAPIs> GetAPIsForCamera(int cameraIndex)
         {
-            using (var captured = new VideoCapture(selectedCamera))
+            List<VideoCaptureAPIs> availableAPIs = new List<VideoCaptureAPIs>();
+
+            var APIs = new List<VideoCaptureAPIs>
+            {
+                VideoCaptureAPIs.ANY, // Automatic Selection - When you don't have a preference OpenCV chooses the best option
+                VideoCaptureAPIs.DSHOW, // DirectShow - Good performance and compability for video capture hardware on Windows
+                VideoCaptureAPIs.MSMF, // Microsoft Media Foundation - Support a wide range of video formats and modern hardware
+                VideoCaptureAPIs.FFMPEG, // FFMpeg backend - popular choice for handling video files
+                VideoCaptureAPIs.IMAGES // OpenCv Image Sequence - For processing a series of still images in a video-like manner
+            };
+
+            foreach(var API in APIs)
+            {
+                using (var caputed = new VideoCapture(cameraIndex, API))
+                {
+                    if (caputed.IsOpened())
+                    {
+                        availableAPIs.Add(API);
+                    }
+                }
+            }
+
+            return availableAPIs;
+        }
+        static void OpenCamera(int selectedCamera, VideoCaptureAPIs selectedAPI) // Add API option later VideoCapture APIs
+        {
+            using (var captured = new VideoCapture(selectedCamera, selectedAPI))
             {
                 if (!captured.IsOpened())
                 {
@@ -79,7 +134,7 @@ namespace AccessingTheCamera
                         break;
                     }
 
-                    Cv2.ImShow("Original Webcam", videoCaptured);
+                    Cv2.ImShow($"Camera {selectedCamera + 1}; API: {selectedAPI}", videoCaptured);
 
                     if(Cv2.WaitKey(1) == 27)
                     {
